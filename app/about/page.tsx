5750d1e2e58628b8b8156ce1480f9b82435ebeb8
nate-leake <nateleakenasa@gmail.com>
2025-12-11 17:51:15 -0600

    Updated text

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,13 +8,15 @@ export default function About() {
             <div className={`${styles.mainGrid}`}>{/* mission & values */}
                 <div className={`generalContainer section`} > {/* our mission */}
                     <div className={`sectionHeader`}>
-                        <p className={`sectionTitle`}>our mission</p>
-                        <p className={`sectionSubTitle`}>to increase the wellbeing of humanity</p>
+                        <p className={`sectionTitle`}>Our Mission</p>
+                        <p className={`sectionSubTitle`}>To increase the wellbeing of humanity</p>
                     </div>
 
                     <div className={`sectionBody`}>
                         <p>
-                            We will focus on creating products that help people in vairous ways. 
+                            We're focused on creating products that help people in various ways.
+                            Our first product is (mood). A mental health app to track tool usage, mood, goals, and progress.
+                            You can find more information about (mood) on our products page.
                         </p>
                         <p>
                             All people are human and we should treat them as such.
@@ -24,9 +26,9 @@ export default function About() {
                 </div>
 
                 <div className={`generalContainer section`}>{/* our values */}
-                    <div className={`$ectionHeader`}>
-                        <p className={`sectionTitle`}>our values</p>
-                        <p className={`sectionSubTitle`}>this is what we beleive in</p>
+                    <div className={`$sectionHeader`}>
+                        <p className={`sectionTitle`}>Our Values</p>
+                        <p className={`sectionSubTitle`}>This is what we believe in</p>
                     </div>
 
                     <div className={`sectionBody`}>
@@ -67,54 +69,42 @@ export default function About() {
 
             <div className={`generalContainer section`}> {/* our story */}
                 <div className={`sectionHeader`}>
-                    <p className={`sectionTitle`}>our story</p>
+                    <p className={`sectionTitle`}>Our Story</p>
                     <p className={`sectionSubTitle`}>
-                        Therapy is a journey you take at your own pace.
+                        Everyone deserves a life they enjoy
                     </p>
                 </div>
 
 
                 <div className={`sectionBody`}>
                     <p>
-                        Where were my emotions at any given moment?
-                        What significant things happened in my life in the last week? What coping tools do I have to use in this hard moment?
-                        Does that coping tool even work for me? What progress am I making?
+                        This business was started by Nate Leake as a hobby. Growing up, Nate had several ideas for new products.
+                        This time, the product actually had some ground under it.
                     </p>
 
                     <p>
-                        This business was started to help people answer those kinds of questions.
-                        While I was in therapy for depression and anxiety I noticed that I had no sense of progress.
-                        I knew I felt better on some days and worse on others, but I couldn’t pinpoint why.
-                        What was making good days good and bad days bad?
+                        The original product idea was (mood) which is still under development. This idea came from Nate's journey in therapy.
+                        He had an idea to create a mood tracking app to show him his progress as he underwent therapy for anxiety and depression.
+                        Luckily, he already knew some coding skills to get a basic app up and running. But why stop with just himself? If this app worked for him, why not allow everyone else to use it?
+                        This was just the beginning though. It turns out starting a business is no easy feat. Especially for a first time entrepreneur.
                     </p>
 
                     <p>
-                        I realized I could keep track of impactful tools, tricks, or even if the sun was shining.
-                        Maybe if I wrote down the things that made me feel happier, calmer, or more relaxed I could try those things when I was anxious, overwhelmed, or depressed.
-                        I could also write down things that didn’t help or feel right when I was struggling.
-                        Then, I could go back to my therapist and report what worked and what didn’t.
-                        I could also track my mood each day and see what kind of trends there were.
+                        After Nate created a basic version of the app (mood), he wanted to learn how to actually distribute it. How do you start a business?
+                        Maybe it was a good idea to attend a business class. That is when Nate found <a className="styledLink" href="https://bizstarts.com/" >BizStarts</a>, a local company in Milwaukee WI.
                     </p>
 
                     <p>
-                        Luckily for me, I already knew some coding.
-                        I decided to start working on an app to do all of this tracking.
-                        Then I could see graphs, add coping tools, give myself reminders, maybe a journaling feature.
-                        I was full of excitement and ideas.
+
                     </p>
 
                     <p>
-                        I didn’t realize it at the time, but this was the start of my business.
-                        This was also a large part of my healing process.
-                        I had a project to work on that I knew could make a difference in my life and maybe others’.
-                        That gave me a sense of direction.
-                        And as I added more features I could see the progress I was making in therapy even though my app didn’t actually change how I was doing therapy.
+
                     </p>
 
                     <p>
-                        I still have a lot of ideas of how to make this app more useful.
-                        Making a product that will help people is my passion.
-                    </p>
+
+                    </ p>
                 </div>
 
 
