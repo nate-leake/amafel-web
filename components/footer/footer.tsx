fe885c7dfcbbd4a54a5507d3d894ecb5976cf3f5
nate-leake <nateleakenasa@gmail.com>
2025-12-12 16:26:15 -0600

    Added Keebo picture

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,10 +1,22 @@
-
+import Image from "next/image"
 export default function Footer() {
     return (
         <div style={{ borderRadius:"20px", margin: "5px"}} className="ultrathick material">
 
             <p style={{textAlign:"center", fontWeight:"bold"}}>made by humans</p>
-            <p style={{padding:"10px", fontSize:"small", textAlign: "center"}}>&copy;2025 mood. all rights reserved.</p>
+            <p style={{padding:"10px", fontSize:"small", textAlign: "center"}}>&copy;2025 Amafel. All rights reserved.</p>
+
+            
+
+            <Image
+              priority
+
+              src="https://drive.google.com/uc?export=download&id=1tyK8nzkMZ6AWLz004adYP7U8H4OcxyZ5"
+              alt="Nate and Keebo"
+              width={25}
+              height={50}
+              style={{ position: "fixed", left: "3px", bottom:"0px" }}
+            />
         </div>
     )
 }
