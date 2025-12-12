9fc321f429df71c226f38c4932ba85dbc64845c8
nate-leake <nateleakenasa@gmail.com>
2025-12-12 13:41:23 -0600

    Changed styling

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,14 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <div className="generalContainer section">
+      <div className="generalContainer section" style={{backgroundColor: "var(--olive-leaf)"}}>
 
-        <div className="sectionHeader">
+        <div className="sectionHeader" style={{color: "var(--cornsilk)"}}>
           <p className="sectionTitle">Welcome to Amafel</p>
           <p className="sectionSubTitle">where humans help humans</p>
         </div>
 
-        <div className="sectionBody">
+        <div className="sectionBody" style={{color: "white"}}>
           <p>Here, we help each other overcome the challenges of life.</p>
         </div>
 
