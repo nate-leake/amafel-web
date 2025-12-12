84e5781cb0ca1a8162e9ae6babf8ecb5429e7272
nate-leake <nateleakenasa@gmail.com>
2025-12-12 16:25:53 -0600

    Added content

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,89 @@
 import Image from "next/image";
+import styles from "./home.module.css"
 
 export default function Home() {
   return (
     <div>
-      <div className="generalContainer section" style={{backgroundColor: "var(--olive-leaf)"}}>
+      <div className="generalContainer  roundBL  roundBR" style={{ backgroundColor: "var(--olive-leaf)" }}>
 
-        <div className="sectionHeader" style={{color: "var(--cornsilk)"}}>
+        <div className="sectionHeader" style={{ color: "var(--cornsilk)" }}>
           <p className="sectionTitle">Welcome to Amafel</p>
-          <p className="sectionSubTitle">where humans help humans</p>
-        </div>
-
-        <div className="sectionBody" style={{color: "white"}}>
-          <p>Here, we help each other overcome the challenges of life.</p>
+          <p className="sectionSubTitle">Where humans help humans</p>
         </div>
 
       </div>
+
+      <div className="generalContainer roundTL" style={{ 
+        backgroundColor: "var(--yale-blue)", 
+        color: "color-mix(in srgb, white 95%, transparent)"
+        }}>
+        <div className="sectionHeader">
+          <p className="sectionTitle">Meet the founder & CEO</p>
+          <p className="sectionSubTitle">Nate Leake</p>
+        </div>
+
+        <div className="sectionBody">
+          <div className={`${styles.wrapper}`} style={{ flexDirection: "row-reverse" }}>
+
+
+            <Image
+              className={`${styles.leftAlignedImage}`}
+              priority
+
+              src="https://drive.google.com/uc?export=download&id=1y6Y9MHrqOHE_3Y9mnHBZDHRgxqYne-36"
+              alt="Nate and Keebo"
+              width={180}
+              height={180}
+              style={{ borderRadius: "7px" }}
+            />
+
+
+            <div className={`${styles.textContent} sectionBody`} style={{marginTop: "0", paddingTop: "0"}} >
+              <p>Hi there, I'm Nate. Don't mind my cat in the photo. We'll get to her in a bit!</p>
+              <p>
+                I'm currently 22 years old and I have a vision for humanity. I want people to have access to mental health resources,
+                financial education, housing, and a life they can enjoy.
+              </p>
+              <p>
+                I know this is an enormous goal. I know I can't do all of this alone. My plan: interdependence. We have to work together to create the world we want.
+                Sometimes I have a hard time asking for help. If you have an idea, or are interested in helping me better humanity, please 
+                let me know.
+              </p>
+
+              <p>
+
+              </p>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="generalContainer section" style={{ backgroundColor: "var(--cornsilk)", color: "black" }}>
+          <p className="sectionSubTitle">The Cat</p>
+          <div className={`${styles.wrapper}`}>
+            <Image
+              className={`${styles.leftAlignedImage}`}
+              priority
+
+              src="https://drive.google.com/uc?export=download&id=1W4EbleCMgjNDUhHaUt7iZQ2UPgxEODXG"
+              alt="Picture a really pretty kitty"
+              width={180}
+              height={180}
+              style={{ borderRadius: "7px" }}
+            />
+
+            <div className={`${styles.textContent}`}>
+              <p>This is Keebo. No, she's not for sale. And yes, she took this picture herself! </p>
+              <p>Keebo is very important!! </p>
+              <p>She might even be sitting on my keyboard as you're reading this!</p>
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+
+
     </div>
   );
 }
